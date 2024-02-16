90e8a3015eaf90e4c5cbca514b25e9848658b8f0
criticalsession <criticalsession@protonmail.ch>
2024-02-16 08:48:14 +0100

    click to replace description

--- main.js
+++ main.js
@@ -48,8 +48,8 @@ function randomizeHeader() {
     html += currHeader[i] === originalHeader[i] ? currHeader[i] : `<span>${currHeader[i]}</span>`;
   }
 
-  if (Math.floor(Math.random() * 100) > 90) {
-    switch (Math.floor(Math.random() * 5)) {
+  if (Math.floor(Math.random() * 100) > 80) {
+    switch (Math.floor(Math.random() * 6)) {
       case 0:
         html = `<span>[</span>${html}<span>]</span>`;
         break;
@@ -57,13 +57,16 @@ function randomizeHeader() {
         html = `<span>></span>${html}`;
         break;
       case 2:
-        html = `<span>${html}</span>`;
+        html = `<span>500: server error</span>`;
         break;
       case 3:
-        html = html.toUpperCase();
+        html = `<span>  (╯°□°)╯︵ ┻━┻ </span>`;
         break;
       case 4:
-        html = `<span>500: server error</span>`;
+        html = `<span>   send help   </span>`;
+        break;
+      case 5:
+        html = `<span>'*.,.*'¨'*.,.*'</span>`;
         break;
     }
   }
@@ -79,14 +82,87 @@ function stopRandom() {
   clearTimeout(timer);
   document.querySelector('h1').innerHTML = 'ok... :(';
   document.querySelector('#stopRandomLink').style.height = 0;
-  document.querySelector('#stopRandomComment').style.height = '30px';
-  setTimeout(randomizeHeader, 1500);
+  setTimeout(() => {
+    document.querySelector('#stopRandomComment').style.height = '30px';
+    randomizeHeader();
+  }, 1500);
 }
 
-function removeDescription() {}
+let replacingDescription = false;
+const originalDescription = `Hi, I'm a guy who likes building things that run on computers &mdash; software, websites, that
+      sort of thing &mdash; and I've been doing that since 2008 (<span id="yearDiff"></span> years ~wow).<br/><br/>I work with
+      C# and Vue primarily, but have recently fallen in love with Go.`;
+function removeDescription() {
+  const cursor = document.getElementById('cursor');
+  if (cursor !== null) {
+    cursor.remove();
+  }
+  const el = document.querySelector('#description');
+  const p = el.innerHTML.split(' ').slice(0, -1);
+  el.innerHTML = p.join(' ') + '<span id="cursor"></span>';
+
+  if (p.length > 0) {
+    setTimeout(removeDescription, 30);
+  } else {
+    setTimeout(fillWithFormalDescription, 500);
+  }
+}
+
+const formalDescription = `Greetings,<br/><br/>
+I am referred to as Amante Reale, alternatively recognized in the digital sphere under the pseudonym criticalsession. Since
+the annum 2008, I have been diligently engaged in the profession of software engineering. Indeed, my experience dates back
+to an era characterized by the utilization of tables in website design, a testament to my extensive tenure in this field. During
+the considerable period spent in the pursuit of my profession, I have acquired a multitude of skills and knowledge, with particular
+emphasis on the development of application programming interfaces (APIs) as well as the creation of practical tools and utilities.
+<br/><br/>
+My work is predominantly conducted utilizing C# and Vue, though I have recently embarked on an exploration of the Go programming
+language. My passions include the composition of command-line interface (CLI) tools, the examination of code authored by
+others, and the meticulous crafting of README files and documentation.`.split(' ');
+let currDescriptionPos = 0;
+function fillWithFormalDescription() {
+  const cursor = document.getElementById('cursor');
+  if (cursor !== null) {
+    cursor.remove();
+  }
+  const el = document.querySelector('#description');
+  const word = formalDescription[currDescriptionPos];
+  el.innerHTML = el.innerHTML + ' ' + word + '<span id="cursor"></span>';
+
+  if (currDescriptionPos < formalDescription.length - 1) {
+    currDescriptionPos++;
+    setTimeout(fillWithFormalDescription, 10);
+    return;
+  }
+
+  const link = document.querySelector('#betterDescriptionLink');
+  link.innerHTML = 'oh god no, go back!';
+  link.onclick = revertDescription;
+  replacingDescription = false;
+  currDescriptionPos = 0;
+}
+
+function revertDescription() {
+  document.querySelector('#description').innerHTML = originalDescription + '<span id="cursor"></span>';
+  workYearDiff();
+  const link = document.querySelector('#betterDescriptionLink');
+  link.innerHTML = 'click here';
+  link.onclick = () => {
+    if (!replacingDescription) {
+      removeDescription();
+    }
+
+    replacingDescription = true;
+  };
+}
 
 workYearDiff();
 setTimeout(randomizeHeader, 1000);
 
 document.querySelector('#stopRandomLink').onclick = stopRandom;
-document.querySelector('#betterDescriptionLink').onclick = removeDescription;
+document.querySelector('#betterDescriptionLink').onclick = () => {
+  if (!replacingDescription) {
+    removeDescription();
+  }
+
+  replacingDescription = true;
+};
